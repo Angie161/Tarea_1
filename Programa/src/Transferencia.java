6812a72784ed08aded3fccd7181d8e16e7398365
Angie161 <aramirez2022@udec.cl>
2023-09-28 18:20:15 -0300

    Actualización de parámetros en contructor de la clase Transferencia

--- Programa/src/Transferencia.java
+++ Programa/src/Transferencia.java
@@ -2,8 +2,8 @@ public class Transferencia extends Pago {
     private String banco;
     private String numCuenta;
 
-    public Transferencia(float monto,OrdenCompra Boleta, String banco,String numCuenta){
-        super(monto,Boleta);
+    public Transferencia(float monto, String banco,String numCuenta){
+        super(monto);
         this.banco = banco;
         this.numCuenta = numCuenta;
     }
