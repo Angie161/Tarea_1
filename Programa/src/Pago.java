ad27bedc794cd6f8c536647189275b914d93f53a
Angie161 <aramirez2022@udec.cl>
2023-09-28 20:47:45 -0300

    Añadida de documentación a la clase Pago

--- Programa/src/Pago.java
+++ Programa/src/Pago.java
@@ -1,32 +1,44 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Clase que permite efectuar el pago de la Orden de compra a través de 3 subclases llamadas Efectivo, Tarjeta y Transferencia.
+ *
+ * @author Angie Ramírez
+ * @version version 2, 28 de Septiembre del 2023
+ */
 public class Pago {
+
+    /** float para almacenar la cantidad de dinero con el que se quiere pagar. */
     private float monto;
+
+    /** Date que guarda la fecha en la que se realiza el pago. */
     private Date fecha;
 
+    /**
+     * Constructor de la clase Pago.
+     * @param monto float con la cantidad de dinero a pagar.
+     */
     public Pago(float monto){
         fecha=new Date();
         this.monto=monto;
     }
 
+    /**
+     * Override del método toString. Devuelve la información del pago.
+     * @return Una cadena que contiene el monto del pago y la fecha de emisión
+     */
     @Override
     public String toString(){
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         return "Monto ingresado para el pago: "+monto+", fecha: "+formato.format(fecha);
     }
 
+    /**
+     * Devuelve el monto almacenado en la variable privada.
+     * @return el monto almacenado.
+     */
     public float GetMonto(){
         return monto;
     }
 }
-
-
-
-
-
-// Cuando se cree un pago se le asociará una orden de compra
-//a través de un método llamado "pagar(Pago Nombredelpago)" en
-//donde se modificará el estado del "estado";
-
-
