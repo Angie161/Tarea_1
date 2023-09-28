524a6c94bd4f6aeedf13a3b36ab9473723f3c471
Angie161 <aramirez2022@udec.cl>
2023-09-28 20:48:12 -0300

    Añadida de documentación a la clase Transferencia

--- Programa/src/Transferencia.java
+++ Programa/src/Transferencia.java
@@ -1,13 +1,32 @@
+/**
+ * Subclase de Pago, la cual permite realizar la compra a través de una transferencia bancaria.
+ *
+ * @author Angie Ramírez
+ * @version 2, 28 de Septiembre del 2023
+ */
 public class Transferencia extends Pago {
+    /** String para almacenar el nombre del banco al que se asocia la transacción*/
     private String banco;
+
+    /** String para almacenar el número de cuenta del Cliente*/
     private String numCuenta;
 
+    /**
+     * Método constructor de la clase Transferencia, que hereda los datos de la clase Pago.
+     * @param monto float con la cantidad de dinero a pagar.
+     * @param banco String en el que se almacena el nombre del banco.
+     * @param numCuenta String donde se guarda el numero de Cuenta.
+     */
     public Transferencia(float monto, String banco,String numCuenta){
         super(monto);
         this.banco = banco;
         this.numCuenta = numCuenta;
     }
 
+    /**
+     * Override del método toString.
+     * @return Una cadena con los datos de la Transferencia otorgados, es decir el banco y número de cuenta.
+     */
     @Override
     public String toString(){
         return "Transferencia desde el banco: "+banco+", Numero de Cuenta: "+numCuenta;
